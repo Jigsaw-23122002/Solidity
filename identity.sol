f2df1ff60d9b45b683da34eaa19d13dcc3ef2a9f
Harsh <harshnag23@gmail.com>
2023-01-14 19:17:00 +0530

    First program in solidity

---
+++ identity.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Identity{
+    string name;
+    uint age;
+
+    constructor() public{
+        name="Harsh Suryanath Nag";
+        age=19;
+    }
+    function getName() view public returns(string memory){
+        return name;
+    }
+    function getAge() view public returns(uint){
+        return age;
+    }
+    function setAge() public{
+        age=age+1;
+    }
+}
