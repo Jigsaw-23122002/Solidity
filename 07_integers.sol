59e9f199f86e712f5d2991fd2a44a1c60780ee17
Harsh <harshnag23@gmail.com>
2023-01-15 12:33:20 +0530

    Integers in solidity

---
+++ 07_integers.sol
@@ -0,0 +1,13 @@
+/*
+Integers in solidity is of two types -
+1) int ---->signed integers
+2) uint ---->unsigned integers
+
+* signed integers are from int8 to int256
+* unsigned integers are from uint8 to uint256
+* default value of all the integers is 0
+* int is alias to int256 and uint is alias to uint256
+* the range of signed integer datatype is -2^(n-1) to 2^(n-1)-1;
+* the range of unsigned integer datataype is 0 to 2^(n-1)-1;
+
+*/
